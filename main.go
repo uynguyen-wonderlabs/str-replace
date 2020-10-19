92642ff3dc8852474fff3e24950dd317ae62deec
Uy Nguyen <70513886+uynguyen-wonderlabs@users.noreply.github.com>
2020-10-19 16:47:11 +0700

    Update main.go

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func main() {
 		}
 	}
 	semiformat := fmt.Sprintf("%q\n", finalResultArray) // Turn the slice into a string that looks like ["one" "two" "three"]
-	tokens := strings.Split(semiformat, " ")	// Split this string by spaces
+	tokens := strings.Split(semiformat, "\" \"")	// Split this string by spaces
 	finalResults := strings.Join(tokens, ", ")
 	
 	fmt.Println(fmt.Sprintf(`::set-output name=fileFoundCount::%d`, fileFoundCount))
